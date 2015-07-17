8a1dc4db557e07e4bf29920a52bbb25aee7c9511
Conner DiPaolo <cdipaolo96@gmail.com>
2015-07-17 23:29:31 +0530

    Took 'y' normalization out because that's not even a thing 😊

--- base/munge.go
+++ base/munge.go
@@ -1,9 +1,6 @@
 package base
 
-import (
-	"fmt"
-	"math"
-)
+import "math"
 
 // Normalize takes in an array of arrays of
 // inputs as well as the corresponding array
@@ -12,36 +9,16 @@ import (
 //
 // That is:
 // x[i][j] := x[i][j] / |x[i]|
-// y[i][j] := y[i][j] / |x[i]|
-//
-// Note that if you don't want to modify the
-// solution values (for instance if you are
-// trying to classify within the Bernoulli set
-// and the solution values are all in {0,1})
-// then you can pass 'true' to the optional
-// 'ignoreY' parameter
-func Normalize(x [][]float64, y [][]float64, ignoreY ...bool) error {
-	if len(x) != len(y) {
-		return fmt.Errorf("ERROR: Length of x (%v) does not equal length of y (%v)", len(x), len(y))
-	}
-
-	ignore := len(ignoreY) != 0 && ignoreY[0]
-
+func Normalize(x [][]float64) {
 	for i := range x {
-		NormalizePoint(x[i], y[i], ignore)
+		NormalizePoint(x[i])
 	}
-
-	return nil
 }
 
 // NormalizePoint is the same as Normalize,
 // but it only operates on one singular datapoint,
 // normalizing it's value to unit length.
-//
-// If 'true' is passed for 'ignoreY', then
-// the y value is not modified. Note that
-// ignoreY is optional.
-func NormalizePoint(x []float64, y []float64, ignoreY ...bool) {
+func NormalizePoint(x []float64) {
 
 	var sum float64
 	for i := range x {
@@ -52,13 +29,5 @@ func NormalizePoint(x []float64, y []float64, ignoreY ...bool) {
 
 	for i := range x {
 		x[i] /= mag
-	}
-
-	ignore := len(ignoreY) != 0 && ignoreY[0]
-
-	if !ignore {
-		for i := range y {
-			y[i] /= mag
-		}
 	}
 }
--- base/munge_test.go
+++ base/munge_test.go
@@ -8,11 +8,9 @@ import (
 
 func TestNormalizeShouldPass1(t *testing.T) {
 	x := [][]float64{}
-	y := [][]float64{}
 
 	for i := -200; i < 200; i++ {
 		row := []float64{}
-		y = append(y, []float64{float64(i)})
 
 		for j := -200; j < 200; j++ {
 			row = append(row, float64(j))
@@ -21,29 +19,21 @@ func TestNormalizeShouldPass1(t *testing.T) {
 		x = append(x, row)
 	}
 
-	err := Normalize(x, y)
-	assert.Nil(t, err, "Normalization error should be nil")
+	Normalize(x)
 
 	for i := range x {
 		for j := range x[i] {
 			assert.True(t, x[i][j] > -1.0001, "X[i][j] must be greater than -1")
 			assert.True(t, x[i][j] < 1.0001, "X[i][j] must be less than 1")
-		}
-
-		for j := range y[i] {
-			assert.True(t, y[i][j] > -1.0001, "Y[i][j] = %v - should be greater than -1", y[i][j])
-			assert.True(t, y[i][j] < 1.0001, "Y[i][j] = %v - should be less than 1", y[i][j])
 		}
 	}
 }
 
 func TestNormalizeShouldPass2(t *testing.T) {
 	x := [][]float64{}
-	y := [][]float64{}
 
 	for i := -200; i < 200; i++ {
 		row := []float64{}
-		y = append(y, []float64{float64(i)})
 
 		for j := -200; j < 200; j++ {
 			row = append(row, float64(j))
@@ -52,8 +42,7 @@ func TestNormalizeShouldPass2(t *testing.T) {
 		x = append(x, row)
 	}
 
-	err := Normalize(x, y, true)
-	assert.Nil(t, err, "Normalization error should be nil")
+	Normalize(x)
 
 	for i := range x {
 		for j := range x[i] {
@@ -61,81 +50,52 @@ func TestNormalizeShouldPass2(t *testing.T) {
 			assert.True(t, x[i][j] < 1.0001, "X[i][j] must be less than 1")
 		}
 
-		for j := range y[i] {
-			assert.Equal(t, float64(i-200), y[i][j], "Y[i][j] should not have changed")
-			assert.Equal(t, float64(i-200), y[i][j], "Y[i][j] should not have changed")
-		}
 	}
-}
-
-func TestNormalizeShouldFail1(t *testing.T) {
-	err := Normalize([][]float64{
-		[]float64{1, 2, 3, 4, 5},
-		[]float64{1, 2, 3, 2, 1},
-	}, [][]float64{
-		[]float64{1, 2, 3, 4, 5},
-	})
-	assert.NotNil(t, err, "Normalization error should not be nil")
 }
 
 func TestNormalizePointShouldPass1(t *testing.T) {
 	x := []float64{}
-	y := []float64{}
 
 	for i := -200; i < 200; i++ {
 		x = append(x, float64(i))
 	}
 
-	NormalizePoint(x, []float64{100.0, -100.0})
+	NormalizePoint(x)
 
 	for i := range x {
 		assert.True(t, x[i] > -1.0001, "X[i] must be greater than -1")
 		assert.True(t, x[i] < 1.0001, "X[i] must be less than 1")
-	}
-
-	for i := range y {
-		assert.True(t, y[i] > -1.0001, "Y[i] should be greater than -1")
-		assert.True(t, y[i] < 1.0001, "Y[i] should be less than 1")
 	}
 }
 
 func TestNormalizePointShouldPass2(t *testing.T) {
 	x := []float64{}
-	y := []float64{100.0, -100.0}
 
 	for i := -200; i < 200; i++ {
 		x = append(x, float64(i))
 	}
 
-	NormalizePoint(x, y, true)
+	NormalizePoint(x)
 
 	for i := range x {
 		assert.True(t, x[i] > -1.0001, "X[i] must be greater than -1")
 		assert.True(t, x[i] < 1.0001, "X[i] must be less than 1")
 	}
 
-	assert.Equal(t, 100.0, y[0], "Y[0] should not have changed")
-	assert.Equal(t, -100.0, y[1], "Y[1] should not have changed")
 }
 
 func TestNormalizePointShouldPass3(t *testing.T) {
 	x := []float64{}
-	y := []float64{}
 
 	for i := -200; i < 200; i++ {
 		x = append(x, float64(i))
 	}
 
-	NormalizePoint(x, []float64{100.0, -100.0}, false)
+	NormalizePoint(x)
 
 	for i := range x {
 		assert.True(t, x[i] > -1.0001, "X[i] must be greater than -1")
 		assert.True(t, x[i] < 1.0001, "X[i] must be less than 1")
-	}
-
-	for i := range y {
-		assert.True(t, y[i] > -1.0001, "Y[i] should be greater than -1")
-		assert.True(t, y[i] < 1.0001, "Y[i] should be less than 1")
 	}
 }
 
@@ -143,25 +103,22 @@ func TestNormalizePointShouldPass3(t *testing.T) {
 
 func BenchmarkNormalizePoint1Input(b *testing.B) {
 	x := []float64{100.0}
-	y := []float64{100.0}
 
 	for i := 0; i < b.N; i++ {
-		NormalizePoint(x, y)
+		NormalizePoint(x)
 	}
 }
 
 func BenchmarkNormalizePoint5Inputs(b *testing.B) {
 	x := []float64{100.0, -12345.5, 10.091238491375, 32.0, 78743.1}
-	y := []float64{100.0}
 
 	for i := 0; i < b.N; i++ {
-		NormalizePoint(x, y)
+		NormalizePoint(x)
 	}
 }
 
 func BenchmarkNormalizePoint50Inputs(b *testing.B) {
 	x := []float64{}
-	y := []float64{100.0}
 
 	for i := 0; i < 50; i++ {
 		x = append(x, float64(i))
@@ -169,13 +126,12 @@ func BenchmarkNormalizePoint50Inputs(b *testing.B) {
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		NormalizePoint(x, y)
+		NormalizePoint(x)
 	}
 }
 
 func BenchmarkNormalizePoint100Inputs(b *testing.B) {
 	x := []float64{}
-	y := []float64{100.0}
 
 	for i := 0; i < 100; i++ {
 		x = append(x, float64(i))
@@ -183,13 +139,12 @@ func BenchmarkNormalizePoint100Inputs(b *testing.B) {
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		NormalizePoint(x, y)
+		NormalizePoint(x)
 	}
 }
 
 func BenchmarkNormalizePoint200Inputs(b *testing.B) {
 	x := []float64{}
-	y := []float64{100.0}
 
 	for i := 0; i < 200; i++ {
 		x = append(x, float64(i))
@@ -197,13 +152,12 @@ func BenchmarkNormalizePoint200Inputs(b *testing.B) {
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		NormalizePoint(x, y)
+		NormalizePoint(x)
 	}
 }
 
 func BenchmarkNormalizePoint300Inputs(b *testing.B) {
 	x := []float64{}
-	y := []float64{100.0}
 
 	for i := 0; i < 300; i++ {
 		x = append(x, float64(i))
@@ -211,13 +165,12 @@ func BenchmarkNormalizePoint300Inputs(b *testing.B) {
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		NormalizePoint(x, y)
+		NormalizePoint(x)
 	}
 }
 
 func BenchmarkNormalizePoint400Inputs(b *testing.B) {
 	x := []float64{}
-	y := []float64{100.0}
 
 	for i := 0; i < 400; i++ {
 		x = append(x, float64(i))
@@ -225,6 +178,6 @@ func BenchmarkNormalizePoint400Inputs(b *testing.B) {
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		NormalizePoint(x, y)
+		NormalizePoint(x)
 	}
 }
