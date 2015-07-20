94b389687381abebd19e1f50f1d737b0c8c90bbf
Conner DiPaolo <cdipaolo96@gmail.com>
2015-07-20 11:02:50 +0200

    Kernel Perceptron implemented with TestLinearKernelOneDXShould... passing

---
+++ perceptron/kernel_perceptron.go
@@ -0,0 +1,269 @@
+package perceptron
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/cdipaolo/goml/base"
+)
+
+// KernelPerceptron represents the perceptron online
+// learning model, where you input features and
+// the model's state reacts to the input and changes
+// weights (parameter vector theta) only when
+// the guess by the algorithm is wrong. Unlike the
+// base Perceptron model, which maps inputs using
+// an affine feature space, the KernelPerceptron
+// can use different kernels which may map to
+// infinite-dimension feature spaces.
+//
+// The hypothesis is a generalized version of the
+// regular perceptron, where i∈M where M are all
+// misclassified examples:
+//      sgn(Σ αy[i] * K(x[i], x))
+//
+// In this implementation, data is passed through a
+// channel, where the learn function is run in a
+// separate goroutine and stops when the channel is
+// closed.
+//
+// http://cs229.stanford.edu/notes/cs229-notes6.pdf
+// https://en.wikipedia.org/wiki/Perceptron
+// https://en.wikipedia.org/wiki/Kernel_perceptron
+//
+// KernelPerceptron implements the OnlineModel interface,
+// not the Model interface, because it uses online
+// learning only
+//
+// Data results in this binary class model are
+// expected to be either -1 or 1 (ie. the
+// base.Datapoint's you pass should, called point,
+// have point.Y be either [-1] or [1])
+//
+// You must pass in a valid kernel function with
+// the NewKernelPerceptron function. You can find
+// premade, valid kernels in the `base` package
+// if you want to use those.
+type KernelPerceptron struct {
+	// SV stores the KernelPerceptron's support
+	// vectors
+	SV []base.Datapoint `json:"support_vectors,omitempty"`
+
+	Kernel func([]float64, []float64) float64
+}
+
+// NewKernelPerceptron takes in a learning rate alpha, the
+// number of features (not including the constant
+// term) being evaluated by the model, the update
+// callback called whenever the perceptron updates the
+// parameter vector theta (whenever it makes a wrong
+// guess), and a channel of datapoints that will be
+// used in training and returns an instantiated model.
+//
+// Again! Features _does not_ include the constant
+// term!
+//
+// Also, learning rate of 0.1 seems to work well in
+// many cases. (I also heard that in a lecture video
+// from a UW professor)
+//
+// Weight is the importance given to newer support
+// vectors in prediction. Should be 0 < w
+func NewKernelPerceptron(kernel func([]float64, []float64) float64) *KernelPerceptron {
+	return &KernelPerceptron{
+		Kernel: kernel,
+	}
+}
+
+// Predict takes in a variable x (an array of floats,) and
+// finds the value of the hypothesis function given the
+// current parameter vector θ
+func (p *KernelPerceptron) Predict(x []float64, normalize ...bool) ([]float64, error) {
+	if len(normalize) != 0 && normalize[0] {
+		base.NormalizePoint(x)
+	}
+
+	var sum float64
+	for i := range p.SV {
+		sum += p.SV[i].Y[0] * p.Kernel(p.SV[i].X, x)
+	}
+
+	result := -1.0
+	if sum > 0 {
+		result = 1
+	}
+
+	return []float64{result}, nil
+}
+
+// OnlineLearn runs off of the datastream within the Perceptron
+// structure. Whenever the model makes a wrong prediction
+// the parameter vector theta is updated to reflect that,
+// as discussed in the documentation for the Perceptron
+// struct itself, and the OnUpdate function is called with
+// the newly updated parameter vector. Learning will stop
+// when the data channel is closed and all remaining
+// datapoints within the channel have been read.
+//
+// The errors channel will be closed when learning is
+// completed so you know when it's done if you're relying
+// on that for whatever reason
+//
+// onUpdate func ([]float64):
+//
+// onUpdate is a function that is called whenever
+// the perceptron updates it's support vectors
+// This acts almost like a callback and
+// passes the newly added support vector
+// as a slice of floats.
+//
+// This might be useful is you want to maintain
+// an up to date persisted model in a database of
+// your choosing and you'd like to update it
+// constantly.
+//
+// This will be spawned into a new goroutine, so
+// don't worry about the function taking a long
+// time, or blocking.
+//
+// If you want to monitor errors happening within
+// this function, just have a channel of errors
+// you send do within this channel, or some other
+// method if it fits your scenario better.
+//
+// NOTE that there is an optional last parameter which,
+// when true, will normalize all data given on the
+// stream. This will potentially help the optimization
+// converge faster. This is given as a parameter because
+// you won't have direct access to the dataset before
+// hand like you would in batch/stochastic settings.
+func (p *KernelPerceptron) OnlineLearn(errors chan error, dataset chan base.Datapoint, onUpdate func([][]float64), normalize ...bool) {
+	if dataset == nil {
+		err := fmt.Errorf("ERROR: Attempting to learn with a nil data stream!\n")
+		fmt.Printf(err.Error())
+		errors <- err
+		close(errors)
+		return
+	}
+
+	if errors == nil {
+		errors = make(chan error)
+	}
+
+	fmt.Printf("Training:\n\tModel: Kernel Perceptron Classifier\n\tOptimization Method: Online Kernel Perceptron\n...\n\n")
+
+	norm := len(normalize) != 0 && normalize[0]
+
+	var point base.Datapoint
+	var more bool
+
+	for {
+		point, more = <-dataset
+
+		if more {
+			// have a datapoint, predict and update!
+			//
+			// Predict also checks if the point is of the
+			// correct dimensions
+			if norm {
+				base.NormalizePoint(point.X)
+			}
+
+			guess, err := p.Predict(point.X)
+			if err != nil {
+				// send the error channel some info and
+				// skip this datapoint
+				errors <- err
+				continue
+			}
+
+			if len(point.Y) != 1 {
+				errors <- fmt.Errorf("The binary perceptron model requires that the data results (y) have length 1 - given %v", len(point.Y))
+				continue
+			}
+
+			// update the parameters if the guess
+			// is wrong
+			if guess[0] != point.Y[0] {
+				p.SV = append(p.SV, point)
+
+				// call the OnUpdate callback with the new vector
+				// appended to a blank slice so the vector is
+				// passed by value and not by reference
+				go onUpdate([][]float64{append(point.X, point.Y...)})
+			}
+
+		} else {
+			fmt.Printf("Training Completed.\n%v\n\n", p)
+			close(errors)
+			return
+		}
+	}
+}
+
+// String implements the fmt interface for clean printing. Here
+// we're using it to print the model as the equation h(θ)=...
+// where h is the perceptron hypothesis model.
+//
+// Note that I'm using the terniary operator to represent
+// the perceptron:
+//     h(θ,x) = Σ y[i]*K(x[i], x`) > 0 ? 1 : 0
+func (p *KernelPerceptron) String() string {
+	return fmt.Sprintf("h(θ,x) = Σ y[i]*K(x[i], x`) > 0 ? 1 : 0\n\tTotal Support Vectors: %v\n", len(p.SV))
+}
+
+// PersistToFile takes in an absolute filepath and saves the
+// parameter vector θ to the file, which can be restored later.
+// The function will take paths from the current directory, but
+// functions
+//
+// The data is stored as JSON because it's one of the most
+// efficient storage method (you only need one comma extra
+// per feature + two brackets, total!) And it's extendable.
+func (p *KernelPerceptron) PersistToFile(path string) error {
+	if path == "" {
+		return fmt.Errorf("ERROR: you just tried to persist your model to a file with no path!! That's a no-no. Try it with a valid filepath")
+	}
+
+	bytes, err := json.Marshal(p)
+	if err != nil {
+		return err
+	}
+
+	err = ioutil.WriteFile(path, bytes, os.ModePerm)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// RestoreFromFile takes in a path to a parameter vector theta
+// and assigns the model it's operating on's parameter vector
+// to that.
+//
+// The path must ba an absolute path or a path from the current
+// directory
+//
+// This would be useful in persisting data between running
+// a model on data, or for graphing a dataset with a fit in
+// another framework like Julia/Gadfly.
+func (p *KernelPerceptron) RestoreFromFile(path string) error {
+	if path == "" {
+		return fmt.Errorf("ERROR: you just tried to restore your model from a file with no path! That's a no-no. Try it with a valid filepath")
+	}
+
+	bytes, err := ioutil.ReadFile(path)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(bytes, &p)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ perceptron/kernel_perceptron_test.go
@@ -0,0 +1,399 @@
+package perceptron
+
+import (
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/cdipaolo/goml/base"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func init() {
+	// create the /tmp/.goml/ dir for persistance testing
+	// if it doesn't already exist!
+	err := os.MkdirAll("/tmp/.goml", os.ModePerm)
+	if err != nil {
+		panic(fmt.Sprintf("You should be able to create the directory for goml model persistance testing.\n\tError returned: %v\n", err.Error()))
+	}
+}
+
+func TestKernelOneDXShouldPass1(t *testing.T) {
+	// create the channel of data and errors
+	stream := make(chan base.Datapoint, 100)
+	errors := make(chan error)
+
+	model := NewKernelPerceptron(base.LinearKernel())
+
+	go model.OnlineLearn(errors, stream, func(supportVector [][]float64) {})
+
+	// start passing data to our datastream
+	//
+	// we could have data already in our channel
+	// when we instantiated the Perceptron, though
+	var points int
+	for i := -20.1; abs(i) > 1; i *= -0.997 {
+		points++
+		if (i-20)/2 > 0 {
+			stream <- base.Datapoint{
+				X: []float64{i - 20},
+				Y: []float64{1.0},
+			}
+		} else {
+			stream <- base.Datapoint{
+				X: []float64{i - 20},
+				Y: []float64{0},
+			}
+		}
+	}
+
+	fmt.Printf("%v Data Points Pushed\n", points)
+
+	// close the dataset
+	close(stream)
+
+	err, more := <-errors
+
+	assert.Nil(t, err, "Learning error should be nil")
+	assert.False(t, more, "There should be no errors returned")
+
+	// test a larger dataset now
+	count := 0
+	wrong := 0
+	for i := -500.0; i < 500; i++ {
+		guess, err := model.Predict([]float64{i})
+		assert.Nil(t, err, "Prediction error should be nil")
+		assert.Len(t, guess, 1, "Guess should have length 1")
+
+		if i/2 > 0 {
+			if guess[0] != 1.0 {
+				wrong++
+			}
+			assert.Equal(t, 1.0, guess[0], "Guess should be 1")
+		} else {
+			if guess[0] != -1.0 {
+				wrong++
+			}
+			assert.Equal(t, -1.0, guess[0], "Guess should be -1")
+		}
+		count++
+	}
+	fmt.Printf("Accuracy: %v\n\tPoints Testes: %v\n\tMisclassifications: %v\n", 100*(1-float64(wrong)/float64(count)), count, wrong)
+}
+
+func TestKernelOneDXShouldFail1(t *testing.T) {
+	// create the channel of data and errors
+	stream := make(chan base.Datapoint, 1000)
+	errors := make(chan error)
+
+	model := NewKernelPerceptron(base.LinearKernel())
+
+	go model.OnlineLearn(errors, stream, func(supportVector [][]float64) {})
+
+	// give invalid data when it should be -1
+	for i := -500.0; abs(i) > 1; i *= -0.99 {
+		if (i-20)/2+10 > 0 {
+			stream <- base.Datapoint{
+				X: []float64{i - 20},
+				Y: []float64{1.0},
+			}
+		} else {
+			stream <- base.Datapoint{
+				X: []float64{i - 20, 0.0, 0.0, 0.0},
+				Y: []float64{-1.0},
+			}
+		}
+	}
+
+	// close the dataset
+	close(stream)
+
+	for {
+		err, more := <-errors
+
+		if more {
+			assert.NotNil(t, err, "Learning error should not be nil")
+		} else {
+			break
+		}
+	}
+}
+
+func TestKernelOneDXShouldFail2(t *testing.T) {
+	// create the channel of data and errors
+	stream := make(chan base.Datapoint, 1000)
+	errors := make(chan error)
+
+	model := NewKernelPerceptron(base.LinearKernel())
+
+	go model.OnlineLearn(errors, stream, func(supportVector [][]float64) {})
+
+	// give invalid data when it should be -1
+	for i := -500.0; abs(i) > 1; i *= -0.99 {
+		if i/10+20 > 0 {
+			stream <- base.Datapoint{
+				X: []float64{i},
+				Y: []float64{1.0},
+			}
+		} else {
+			stream <- base.Datapoint{
+				X: []float64{i},
+				Y: []float64{-1.0, 10, 10, 10},
+			}
+		}
+	}
+
+	// close the dataset
+	close(stream)
+
+	for {
+		err, more := <-errors
+
+		if more {
+			assert.NotNil(t, err, "Learning error should not be nil")
+		} else {
+			break
+		}
+	}
+}
+
+func TestKernelOneDXShouldFail3(t *testing.T) {
+	// create the channel of errors
+	errors := make(chan error)
+
+	model := NewKernelPerceptron(base.LinearKernel())
+
+	go model.OnlineLearn(errors, nil, func(supportVector [][]float64) {})
+
+	for {
+		err, more := <-errors
+
+		if more {
+			assert.NotNil(t, err, "Learning error should not be nil")
+		} else {
+			break
+		}
+	}
+}
+
+func TestKernelFourDXShouldPass1(t *testing.T) {
+	// create the channel of data and errors
+	stream := make(chan base.Datapoint, 100)
+	errors := make(chan error)
+
+	var updates int
+
+	model := NewPerceptron(0.1, 4)
+
+	go model.OnlineLearn(errors, stream, func(theta [][]float64) {
+		updates++
+	})
+
+	var iter int
+	for i := -200.0; abs(i) > 1; i *= -0.82 {
+		for j := -200.0; abs(j) > 1; j *= -0.82 {
+			for k := -200.0; abs(k) > 1; k *= -0.82 {
+				for l := -200.0; abs(l) > 1; l *= -0.82 {
+					if i/2+2*k-4*j+2*l+3 > 0 {
+						stream <- base.Datapoint{
+							X: []float64{i, j, k, l},
+							Y: []float64{1.0},
+						}
+					} else {
+						stream <- base.Datapoint{
+							X: []float64{i, j, k, l},
+							Y: []float64{-1.0},
+						}
+					}
+
+					iter++
+				}
+			}
+		}
+	}
+
+	// close the dataset
+	close(stream)
+
+	err, more := <-errors
+	assert.Nil(t, err, "Learning error should be nil")
+	assert.False(t, more, "There should be no errors returned")
+
+	assert.True(t, updates > 100, "There should be more than 100 updates of theta")
+
+	for i := -200.0; i < 200; i += 100 {
+		for j := -200.0; j < 200; j += 100 {
+			for k := -200.0; k < 200; k += 100 {
+				for l := -200.0; l < 200; l += 100 {
+					guess, err := model.Predict([]float64{i, j, k, l})
+					assert.Nil(t, err, "Prediction error should be nil")
+					assert.Len(t, guess, 1, "Guess should have length 1")
+
+					if i/2+2*k-4*j+2*l+3 > 0 {
+						assert.Equal(t, 1.0, guess[0], "Guess should be 1")
+					} else {
+						assert.Equal(t, -1.0, guess[0], "Guess should be -1")
+					}
+				}
+			}
+		}
+	}
+}
+
+func TestKernelTwoDXNormalizedShouldPass1(t *testing.T) {
+	// create the channel of data and errors
+	stream := make(chan base.Datapoint, 100)
+	errors := make(chan error)
+
+	var updates int
+
+	model := NewPerceptron(0.1, 2)
+
+	go model.OnlineLearn(errors, stream, func(theta [][]float64) {
+		updates++
+	}, true)
+
+	for i := -200.0; abs(i) > 1; i *= -0.981 {
+		for j := -200.0; abs(j) > 1; j *= -0.981 {
+			x := []float64{i, j}
+			base.NormalizePoint(x)
+
+			if 5*x[0]+10*x[1]-4 > 0 {
+				stream <- base.Datapoint{
+					X: x,
+					Y: []float64{1.0},
+				}
+			} else {
+				stream <- base.Datapoint{
+					X: x,
+					Y: []float64{-1.0},
+				}
+			}
+		}
+	}
+
+	// close the dataset
+	close(stream)
+
+	for {
+		err, more := <-errors
+		assert.False(t, more, "There should not be any errors!")
+
+		if more {
+			assert.Nil(t, err, "Learning error should be nil")
+		} else {
+			break
+		}
+	}
+
+	assert.True(t, updates > 50, "There should be more than 50 updates of theta (%v updates recorded)", updates)
+
+	var count int
+	var incorrect int
+
+	for i := -200.0; abs(i) > 1; i *= -0.85 {
+		for j := -200.0; abs(j) > 1; j *= -0.85 {
+			x := []float64{i, j}
+			base.NormalizePoint(x)
+
+			guess, err := model.Predict([]float64{i, j}, true)
+			assert.Nil(t, err, "Prediction error should be nil")
+			assert.Len(t, guess, 1, "Guess should have length 1")
+
+			if 5*x[0]+10*x[1]-4 > 0 && guess[0] != 1.0 {
+				incorrect++
+			} else if 5*x[0]+10*x[1]-4 <= 0 && guess[0] != -1.0 {
+				incorrect++
+			}
+
+			count++
+		}
+	}
+
+	fmt.Printf("Predictions: %v\n\tIncorrect: %v\n\tAccuracy Rate: %v percent\n", count, incorrect, 100*(1.0-float64(incorrect)/float64(count)))
+	assert.True(t, float64(incorrect)/float64(count) < 0.14, "Accuracy should be greater than 86%")
+}
+
+func TestKernelPersistPerceptronShouldPass1(t *testing.T) {
+	// create the channel of data and errors
+	stream := make(chan base.Datapoint, 100)
+	errors := make(chan error)
+
+	model := NewPerceptron(0.1, 1)
+
+	go model.OnlineLearn(errors, stream, func(supportVector [][]float64) {})
+
+	// start passing data to our datastream
+	//
+	// we could have data already in our channel
+	// when we instantiated the Perceptron, though
+	for i := -500.0; abs(i) > 1; i *= -0.997 {
+		if 10+(i-20)/2 > 0 {
+			stream <- base.Datapoint{
+				X: []float64{i - 20},
+				Y: []float64{1.0},
+			}
+		} else {
+			stream <- base.Datapoint{
+				X: []float64{i - 20},
+				Y: []float64{0},
+			}
+		}
+	}
+
+	// close the dataset
+	close(stream)
+
+	err, more := <-errors
+	assert.Nil(t, err, "Learning error should be nil")
+	assert.False(t, more, "There should be no errors returned")
+
+	// test a larger dataset now
+	for i := -500.0; i < 500; i++ {
+		guess, err := model.Predict([]float64{i})
+		assert.Nil(t, err, "Prediction error should be nil")
+		assert.Len(t, guess, 1, "Guess should have length 1")
+
+		if i/2+10 > 0 {
+			assert.Equal(t, 1.0, guess[0], "Guess should be 1")
+		} else {
+			assert.Equal(t, -1.0, guess[0], "Guess should be -1")
+		}
+	}
+
+	// now persist to file
+	err = model.PersistToFile("/tmp/.goml/Perceptron.json")
+	assert.Nil(t, err, "Persistance error should be nil")
+
+	model.Parameters = make([]float64, len(model.Parameters))
+
+	// make sure it WONT work now that we reset theta
+	//
+	// the result of Theta transpose * X should always
+	// be 0 because theta is the zero vector right now.
+	for i := -40; i < 40; i++ {
+		guess, err := model.Predict([]float64{float64(i)})
+		assert.Len(t, guess, 1, "Length of a Perceptron model output from the hypothesis should always be a 1 dimensional vector. Never multidimensional.")
+		assert.Equal(t, -1.0, guess[0], "Guess should be 0 when theta is the zero vector")
+		assert.Nil(t, err, "Prediction error should be nil")
+	}
+
+	// restore from file
+	err = model.RestoreFromFile("/tmp/.goml/Perceptron.json")
+	assert.Nil(t, err, "Persistance error should be nil")
+
+	// test with original data
+	// test a larger dataset now
+	for i := -500.0; i < 500; i++ {
+		guess, err := model.Predict([]float64{i})
+		assert.Nil(t, err, "Prediction error should be nil")
+		assert.Len(t, guess, 1, "Guess should have length 1")
+
+		if i/2+10 > 0 {
+			assert.Equal(t, 1.0, guess[0], "Guess should be 1")
+		} else {
+			assert.Equal(t, -1.0, guess[0], "Guess should be -1")
+		}
+	}
+}
